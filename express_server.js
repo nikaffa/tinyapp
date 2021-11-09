dfe0900fe21b3f01a1a4ff3cf6d73ffc74205df4
Veronika Fatikhova <fidelio@list.ru>
2021-11-08 19:40:06 -0800

    urls_show.ejs template added

--- express_server.js
+++ express_server.js
@@ -16,6 +16,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase }; //TODO:what's a longURL
+  res.render("urls_show", templateVars);
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
