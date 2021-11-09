9f5079c62a15f184f4f1b602890717f11a1de2c4
Veronika Fatikhova <fidelio@list.ru>
2021-11-08 19:44:23 -0800

    /urls/:shortURL route modified and tested

--- express_server.js
+++ express_server.js
@@ -17,7 +17,7 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase }; //TODO:what's a longURL
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
