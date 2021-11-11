2c58a04694c6e8ee01d9bacbba27191fe9a047e7
Veronika Fatikhova <fidelio@list.ru>
2021-11-10 19:32:08 -0800

    Registration errors implemented

--- express_server.js
+++ express_server.js
@@ -11,9 +11,17 @@ app.use(morgan('dev'));
 //instead of bodyparse using express, extended:false means handle only the value from the client and nothing else
 app.use(express.urlencoded({ extended: false}));
 
-//returns a string of 6 random alphanumeric characters:
-const generateRandomString = () => {
-  return Math.random().toString(36).substring(2,8);
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
 };
 
 const urlDatabase = {
@@ -21,38 +29,84 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+//returns a string of 6 random alphanumeric characters:
+const generateRandomString = () => {
+  return Math.random().toString(36).substring(2,8);
+};
+
+const checkEmail = (email, password) => {
+  if (!email.length || !password.length) {
+    return true;
+  }
+  for (let user in users) {
+    if (email === users[user].email) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+app.get("/", (req, res) => {
+  res.redirect(`/register`);
+});
+
 //READ registration form
 app.get("/register", (req, res) => {
   res.render("register");
 });
-app.get("/", (req, res) => {
-  res.redirect(`/urls`);
+
+//EDIT registration form
+app.post("/register", (req, res) => {
+  //checking errors
+  if (checkEmail(req.body.email, req.body.password))  {
+    res.statusCode = 404;
+    return res.send("Error with email or password");
+  }
+  const newId = generateRandomString();
+  //saving user to database
+  users[newId] = {
+    id: newId,
+    email: req.body.email,
+    password: req.body.password
+  };
+  console.log(users);
+  //set a user_id cookie containing the user's newId
+  res.cookie("user_id", users[newId].id);
+  res.redirect("/urls");
 });
 
 //READ all urls
 app.get("/urls", (req, res) => {
-  //TODO checks value of cookie and don't display database unless logged in
-  const username = req.cookies;
-
-  const templateVars = {
-    username: req.cookies["username"],
-    urls: urlDatabase
-  };
-  console.log(templateVars);
-  res.render("urls_index", templateVars);
+  //TODO!!! checks value of cookie and don't display database unless logged in
+  if (req.cookies["user_id"]) {
+    const user = users[req.cookies["user_id"]];
+    const templateVars = {
+      username: req.cookies["user_id"],
+      email: user.email,
+      urls: urlDatabase
+    };
+    res.render("urls_index", templateVars);
+  }
+  res.redirect("/register");
 });
 
 //READ a form to submit a new url
 app.get("/urls/new", (req, res) => {
-  const templateVars = {
-    username: req.cookies["username"],
-  };
-  res.render("urls_new", templateVars);
+  if (req.cookies["user_id"]) {
+    const user = users[req.cookies["user_id"]];
+    const templateVars = {
+      username: req.cookies["user_id"],
+      email: user.email,
+    };
+    res.render("urls_new", templateVars);
+  }
+  res.redirect("/register");
 });
 
 //EDIT create a new shortURL after submission the form, saves it to database and redirects to its page
 app.post("/urls", (req, res) => {
-  let shortURL = generateRandomString();
+  const shortURL = generateRandomString();
   urlDatabase[shortURL] = req.body.longURL; //shortURL-longURL key-value pair are saved to the urlDatabase
   res.redirect(`/urls/${shortURL}`);
 });
@@ -63,27 +117,31 @@ app.post("/login", (req, res) => {
   const username = req.body;
   console.log('username', username);
   //set a cookie named username to the value submitted in the login form
-  res.cookie("username", username);
+  res.cookie("user_id", username);
   res.redirect("/urls");
 });
 
 //EDIT logging out
 app.post("/logout", (req, res) => {
   //clear cookie and logout
-  res.clearCookie("username");
+  res.clearCookie("user_id");
   return res.redirect("/urls");
 });
 
 //READ a new created shortURL after submission the NewURL form
 app.get("/urls/:shortURL", (req, res) => {
-  console.log('req.param', req.params);
-  const templateVars = {
-    username: req.cookies["username"],
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL]
-  };
-  res.render("urls_show", templateVars);
+  if (req.cookies["user_id"]) {
+    const user = users[req.cookies["user_id"]];
+    const templateVars = {
+      username: req.cookies["user_id"],
+      email: user.email,
+      shortURL: req.params.shortURL,
+      longURL: urlDatabase[req.params.shortURL]
+    };
+    res.render("urls_show", templateVars);
   //res.redirect("/urls");
+  }
+  res.redirect(`/register`);
 });
 
 //TODO: EDIT a single URL - what's after submission?
